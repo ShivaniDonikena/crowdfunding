f7b7fffe2c641494ce0fbf2b635df82ecd9ce8c4
ShivaniDonikena <donikenashivani5@gmail.com>
2025-02-22 15:59:50 +0530

    Add files via upload

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+    return(
+        <div><h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident maxime at sunt quia, dolore cumque facilis saepe porro modi placeat repellat inventore suscipit eos doloribus deserunt, sint eligendi rerum ea?
+        Maxime repellendus voluptates, reiciendis consectetur possimus explicabo qui fuga dicta dolorem sunt at nemo laudantium dolor aspernatur nesciunt nostrum ab earum obcaecati eaque expedita itaque veniam tempore iusto nihil. Eos.
+        Nulla rerum numquam commodi placeat illo aperiam quasi amet praesentium, nisi ipsum harum nobis beatae animi veritatis necessitatibus! Cum quia quasi omnis veniam quae, beatae ratione vero eveniet perferendis nulla.
+        Voluptatum unde, aut deserunt dolorum minus illo exercitationem velit nihil maxime? Iusto voluptatum ad quia saepe quis enim? Sint, quod eaque? Neque, dolorem nulla sint corporis illo consequuntur laudantium impedit?
+        Minus ex nisi, deserunt accusamus placeat veritatis amet ipsam quisquam debitis inventore atque! Vitae, eligendi. Tempore omnis numquam amet aliquam nihil vel, qui error autem minima vero debitis illum voluptates.</h5></div>
+    )
+}
+
+export default Contact
